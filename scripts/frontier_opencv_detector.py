0da4a777030f0415fbc30be2153590419b91d81f
Shubham <skpawar1305@gmail.com>
2024-07-09 17:45:35 +0200

    create and publish costmap

--- scripts/frontier_opencv_detector.py
+++ scripts/frontier_opencv_detector.py
@@ -26,14 +26,44 @@ from geometry_msgs.msg import PointStamped
 from getfrontier import getfrontier
 import numpy as np
 
+EXPANSION_SIZE = 2
+ROBOT_RADIUS = 0.3
+
 
 class OpenCVFrontierDetector(Node):
     def __init__(self):
         super().__init__('detector')
         self.create_subscription(OccupancyGrid, '/projected_map_1m', self.mapCallBack, 1)
         self.targetspub = self.create_publisher(MarkerArray, "/detected_markers", 1)
+
+        self.inflated_map_pub = self.create_publisher(OccupancyGrid, "/projected_map_1m_inflated", 1)
         
         self.create_timer(1, self.timer_callback)
+
+    def costmap(self, map_data: OccupancyGrid) -> OccupancyGrid:
+        width = map_data.info.width
+        height = map_data.info.height
+        data = np.array(map_data.data).reshape((height, width))
+
+        # Find walls (occupied cells)
+        walls = np.where(data == 100)
+
+        for i in range(-EXPANSION_SIZE,EXPANSION_SIZE+1):
+            for j in range(-EXPANSION_SIZE,EXPANSION_SIZE+1):
+                if i  == 0 and j == 0:
+                    continue
+                x = walls[0]+i
+                y = walls[1]+j
+                x = np.clip(x,0,height-1)
+                y = np.clip(y,0,width-1)
+                data[x,y] = 100
+
+        # Convert back to int8 array
+        flattened_data = data.flatten().astype(np.int8)
+
+        # Update the map_data with the expanded map
+        map_data.data = flattened_data.tolist()  # Convert numpy array to list of int8
+        return map_data
 
     def get_nearest_free_space(self, map_data, frontier):
         width = map_data.info.width
@@ -95,6 +125,9 @@ class OpenCVFrontierDetector(Node):
 
     def mapCallBack(self, data):
         self.mapData = data
+
+        self.inflated_map = self.costmap(data)
+        self.inflated_map_pub.publish(self.inflated_map)
 
 def main(args=None):
     rclpy.init(args=args)
